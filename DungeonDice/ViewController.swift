fff55098288b605d6dec1d40808da854a18b3beb
Song San <songsan2012@gmail.com>
2021-12-26 07:49:38 -0800

    Initial Commit

---
+++ DungeonDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DungeonDice
+//
+//  Created by song on 12/26/21.
+//  Copyright © 2021 song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
