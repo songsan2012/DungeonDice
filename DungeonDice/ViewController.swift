307edd7f2258b78d92b77ab6ad8898bc0e2c7607
Song San <songsan2012@gmail.com>
2021-12-26 09:37:53 -0800

    Works returning the roll dice results

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // -- Reset the title and result
+        resultLabel.text = ""
     }
+    
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        
+        // -- Get the Tag
+        let diceTag = sender.tag
+        var rollResult = 0
+      
+        rollResult = Int.random(in: 1...diceTag)
 
-
+        let resultText = "You rolled a \(diceTag) dice and got \(rollResult)"
+        resultLabel.text = resultText
+    }
+    
 }
 
